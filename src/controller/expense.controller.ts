53f63a10498123baa9811cc0b99aa797c90c74a9
saket5743 <saketkumar.93864@gmail.com>
2024-11-26 22:09:24 +0530

    Updated

--- src/controller/expense.controller.ts
+++ src/controller/expense.controller.ts
@@ -5,6 +5,7 @@ import Expense from "../models/expense.models";
 import ApiError from "../errors/ApiError";
 import { BOOL_TRUE, CODE_200, CODE_400, EXPENSE_FOUND, EXPENSE_NOT_CRT, EXPENSE_UPDTD, EXPENSES_NOT_FOUND, SUCCESS } from "../utils/constants";
 import ApiResponse from "../errors/ApiResponseCode";
+import { title } from "process";
 
 // Creating Expense
 export const createExpense = asyncWrapper(async (req: Request, res: Response) => {
@@ -59,3 +60,76 @@ export const deleteExpense = asyncWrapper(async (req: Request, res: Response) =>
   return res.json(new ApiResponse(CODE_200, {}, EXPENSE_UPDTD, BOOL_TRUE))
 })
 
+// RegEx Get Expense
+interface IQueryObj {
+  title?: QueryString.ParsedQs;
+  sort?: string;
+  select?: number;
+  numericFilters?: string;
+  category?: QueryString.ParsedQs
+}
+
+export const getAllExpenseRegex = asyncWrapper(async (req: Request, res: Response) => {
+  const { title, category, sort, select, numericFilters } = req.query;
+  const queryObject: IQueryObj = {}
+
+  if (title) {
+    queryObject.title = { $regex: title, $options: "i" }
+  }
+
+  if (category) {
+    queryObject.category = { $regex: category, $options: "i" }
+  }
+
+  if (numericFilters) {
+    const operatorMap: {
+      ">": string,
+      ">=": string,
+      "=": string,
+      "<": string,
+      "<=": string
+    } = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte"
+    }
+    const reqEx = /\b(<=|>=|<|=|>)/g;
+    let filters
+    if (typeof numericFilters === "string") {
+      filters = numericFilters.replace(reqEx, (match: string) => `-${operatorMap[match as keyof typeof operatorMap]}-`);
+    }
+    const options = ["amount"];
+
+    filters = filters!.split(",").forEach((item: string) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        (queryObject as any)[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  let result = Expense.find(queryObject);
+
+  if (typeof sort === "string") {
+    let sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (typeof select === 'string') {
+    let selectList = select.split(",").join(" ");
+    result = result.select(selectList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const expense = await result;
+  return res.json(new ApiResponse(CODE_200, expense, SUCCESS, BOOL_TRUE))
+})
--- src/router/expense.router.ts
+++ src/router/expense.router.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createExpense, deleteExpense, expenseById, getAllExpense, updateExpense } from '../controller/expense.controller';
+import { createExpense, deleteExpense, expenseById, getAllExpense, getAllExpenseRegex, updateExpense } from '../controller/expense.controller';
 const router = express.Router();
 
 router.route('/getAllExpense').get(getAllExpense)
 router.route('/createExpense').post(createExpense)
 router.route('/:id').get(expenseById).put(updateExpense).delete(deleteExpense)
+router.route('/').get(getAllExpenseRegex)
 
 export default router;
